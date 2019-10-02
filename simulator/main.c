1fe603c4d4447281fbb1665f149fba1b3f11518c
Peder Bergebakken Sundt <pbsds@hotmail.com>
2019-10-02 22:01:56 +0200

    Add ability to print simulator events as command, rename sim.out to main.out

--- simulator/main.c
+++ simulator/main.c
@@ -3,34 +3,35 @@
 #include <string.h>
 #include <stdlib.h>
 
+// runtime flags:
 bool enable_spi_dump        = false;
 bool enable_n_samples_dump  = false;
 bool enable_sample_dump     = false;
 bool enable_raw_sample_dump = false;
-
-/*
-TODO:
- - read midi (preferably only with a header-only library)
- - make the microcontroller code process the events
- - pass events to fpga
- - step the fpga each time we see a midifile time-step event
- - write sound output to stdout/file
-*/
+bool enable_command_style_dump = false;
 
 // a print statement which is able to print even while outputting raw PCM data to stdout:
 #define print(...) {if (enable_raw_sample_dump) {fprintf(stderr, __VA_ARGS__); fflush(stderr);} else {printf(__VA_ARGS__);}}
-
 
 // define missing functions in reference implementation:
 
 // hook the two parts of the reference implementation together:
 void microcontroller_send_spi_packet(const byte* data, size_t length) {
 	if (enable_spi_dump) {
-		print("SPI packet:");
-		for (size_t i = 0; i < length; i++) {
-			print(" %02X", data[i]);
+		if (enable_command_style_dump) {
+			print("send_spi([");
+			for (size_t i = 0; i < length; i++) {
+				if (i) print(", ");
+				print("0x%02X", data[i]);
+			}
+			print("])\n");
+		} else {
+			print("SPI packet:");
+			for (size_t i = 0; i < length; i++) {
+				print(" %02X", data[i]);
+			}
+			print("\n");
 		}
-		print("\n");
 	}
 	fflush(stderr);
 	fpga_handle_spi_packet(data, length);
@@ -48,10 +49,12 @@ bool microcontroller_poll_pcb_button_state(uint button_id) {
 	microcontroller_handle_midi_event((const byte*) data, length)
 
 void generate_samples(size_t n) {
-	if (enable_n_samples_dump)  print("Step %d samples\n", n);
+	if (enable_n_samples_dump &&  enable_command_style_dump) print("step_n_samples(%d)\n", n);
+	if (enable_n_samples_dump && !enable_command_style_dump) print("Step %d samples\n", n);
 	for (size_t i = 0; i < n; i++) {
 		WSample s = fpga_generate_sound_sample();
-		if (enable_sample_dump)     print("Sample: %i\n", s);
+		if (enable_sample_dump &&  enable_command_style_dump) print("expect_sample(%i)\n", s);
+		if (enable_sample_dump && !enable_command_style_dump) print("Sample: %i\n", s);
 		if (enable_raw_sample_dump) { // little endian 32bit signed output
 			printf("%c", *(((byte*)&s)+0));
 			printf("%c", *(((byte*)&s)+1));
@@ -61,7 +64,7 @@ void generate_samples(size_t n) {
 	}
 }
 
-// load in the events created by python script
+// load in the events created by our python script
 void simulate() { // 2hacky4u
 #include "song.c"
 }
@@ -73,6 +76,7 @@ int main(int argc, char const *argv[]) {
 		else if (!strcmp(argv[i], "-n")) enable_n_samples_dump  = true;
 		else if (!strcmp(argv[i], "-o")) enable_sample_dump     = true;
 		else if (!strcmp(argv[i], "-r")) enable_raw_sample_dump = true;
+		else if (!strcmp(argv[i], "-c")) enable_command_style_dump = true;
 	}
 
 	microcontroller_global_generator_state.master_volume = 0xFF;
--- simulator/simulator.py
+++ simulator/simulator.py
@@ -13,13 +13,13 @@ def run(cmd, *args, **kwargs):
 	if isinstance(cmd, str):
 		cmd = split(cmd)
 	print_status(f"+ {' '.join(map(quote, cmd))}")
-	return subprocess.run(cmd, *args, **kwargs)
+	return subprocess.run(cmd, *args, check=True, **kwargs)
 
-def convert_mido_midi_to_c_simulator_events(filename):
+def convert_midi_to_c_simulator_events(filename):
 	t = 0
 	n_samples = 0
 	for msg in MidiFile(filename):
-		if msg.time > 0:
+		if msg.time > 0: # round-about way of tracking time to avoid cumulating rounding errors
 			t += msg.time
 			n = int(t * SAMPLERATE)
 			yield f"generate_samples({n - n_samples});"
@@ -34,43 +34,52 @@ def write_song_c(lines):
 	with open("song.c", "w") as f:
 		f.write("\n".join(lines) + "\n")
 
+def show_help():
+	print(" "*3, __file__, "<midifile> [flags]")
+	print("flags:")
+	print("\t-h   show this")
+	print("\t-p   play output (using APLAY)")
+	print("\t-w   make wav (using SOX)")
+	print("\t-3   make mp3 (using LAME)")
+	print("\t-c   dump as commands instead of text")
+	print("\t-s   enable spi dump")
+	print("\t-n   enable n samples dump")
+	print("\t-o   enable sample dump")
+	print("\t-r   enable raw sample dump")
+
 def main():
-	if len(sys.argv) <= 1:
-		print(" "*3, __file__, "<midifile> [flags]")
-		print("flags:")
-		print("\t-p   play output")
-		print("\t-w   make wav (using SOX)")
-		print("\t-3   make mp3 (using LAME)")
-		print("\t-s   enable spi dump")
-		print("\t-n   enable n samples dump")
-		print("\t-o   enable sample dump")
-		print("\t-r   enable raw sample dump")
+	if len(sys.argv) <= 2:
+		show_help()
 		return
 
 	filename = sys.argv[1]
 	flags = sys.argv[2:]
 
+	if "-h" in flags:
+		show_help()
+		return
+
 	if filename != "-":
 		print_status("Parsing", filename, "into c...")
-		events = convert_mido_midi_to_c_simulator_events(sys.argv[1])
+		events = convert_midi_to_c_simulator_events(sys.argv[1])
 		print_status("Writing song.c...")
 		write_song_c(events)
 
 	print_status("Compiling simulator...")
-	run("gcc main.c -lm -o sim.out") # compile
+	run("gcc main.c -lm -o main.out") # compile
 
 	print_status("Running simulator...")
 	safe_flags = " ".join(quote(i) for i in flags if i not in ("-p", "-w", "-3", "-r"))
 	if "-p" in flags:
-		run(["bash", "-c", f"./sim.out -r {safe_flags} | aplay -c 1 -f S32_LE -r 44100"])
+		run(["bash", "-c", f"./main.out -r {safe_flags} | aplay -c 1 -f S32_LE -r 44100"])
 	elif "-w" in flags:
-		run(["bash", "-c", f"./sim.out -r {safe_flags} | sox -t raw -r 44100 -e signed -b 32 -c 1 - -r 44100 {quote(filename+'.wav')}"])
+		run(["bash", "-c", f"./main.out -r {safe_flags} | sox -t raw -r 44100 -e signed -b 32 -c 1 - -r 44100 {quote(filename+'.wav')}"])
 		print_status(f"output written to {filename+'.wav'}")
 	elif "-3" in flags:
-		run(["bash", "-c", f"./sim.out -r {safe_flags} | lame -r -s 44.1 --bitwidth 32 --signed -m mono - {quote(filename+'.mp3')}"])
+		run(["bash", "-c", f"./main.out -r {safe_flags} | lame -r -s 44.1 --bitwidth 32 --signed -m mono - {quote(filename+'.mp3')}"])
 		print_status(f"output written to {filename+'.mp3'}")
 	else:
-		run(["./sim.out", *flags])
+		run(["./main.out", *flags])
 
 if __name__ == "__main__":
 	main()
